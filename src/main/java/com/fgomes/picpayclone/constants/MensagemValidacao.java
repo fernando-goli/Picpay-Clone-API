417a38d4b1c182a630d3d84febe0f7e2f367d46b
fernando <fernando.gmsoli@gmail.com>
2022-02-16 15:35:53 -0300

    Create DTOs, Enums, constants

---
+++ src/main/java/com/fgomes/picpayclone/constants/MensagemValidacao.java
@@ -0,0 +1,9 @@
+package com.fgomes.picpayclone.constants;
+
+public class MensagemValidacao {
+
+    public static final String ERRO_USUARIO_INEXISTENTE = "O usuário informado não existe na base de dados.";
+    public static final String ERRO_USUARIO_SEM_PERMISSAO = "O usuário não tem permissão para se autenticar.";
+
+
+}
